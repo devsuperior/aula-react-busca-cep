aae3d46c75e30626126a35f2bccb68731d24b9fa
Washington Soares <washingtonudi@gmail.com>
2021-04-06 22:23:17 -0300

    Add classroom files

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
+const BASE_URL = 'https://viacep.com.br/ws'
+
+type Address = {
+  logradouro: string;
+  localidade: string;
+}
+
 function App() {
+  const [searchValue, setSearchValue] = useState('');
+  const [addressData, setAddressData] = useState<Address>();
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setAddressData(undefined);
+
+    axios(`${BASE_URL}/${searchValue}/json`)
+      .then(response => setAddressData(response.data))
+      .catch(() => console.error('Houve um errro ao buscar os dados!'));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-wrapper">
+      <div className="container">
+        <h1 className="title">Busca CEP</h1>
+        <form className="search-form" onSubmit={handleSubmit}>
+          <input
+            type="number"
+            className="search-input"
+            placeholder="CEP (somente números)"
+            value={searchValue}
+            onChange={event => setSearchValue(event.target.value)}
+            maxLength={8}
+          />
+          <button className="search-button">Buscar</button>
+          {addressData && (
+            <>
+              <div className="search-result-item">
+                <strong className="result-title">Logradouro</strong> <br />
+                <span className="result-subtitle">{addressData.logradouro}</span>
+              </div>
+              <div className="search-result-item">
+                <strong className="result-title">Localidade</strong> <br />
+                <span className="result-subtitle">{addressData.localidade}</span>
+              </div>
+            </>
+          )}
+        </form>
+      </div>
     </div>
   );
 }
